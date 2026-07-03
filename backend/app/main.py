95e1cde5d316145d5dcd0a6d32844e17902858f2
Ruchika Verma <2809ruchika@gmail.com>
2026-07-03 17:37:37 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,6 +46,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://healthcareapp-1-g5o7.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
